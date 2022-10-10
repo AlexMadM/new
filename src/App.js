59e14f69a39d67f22984156f25c37b878f1ea8ab
Alex <alex.alexandrov.1988@gmail.com>
2022-10-10 21:18:47 +0300

    search

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
         super(props);
         this.state = {
             monsters: [],
+            searchField:''
         };
 
     }
@@ -19,21 +20,21 @@ class App extends Component {
     }
 
     render() {
+        const filteredMonsters = this.state.monsters.filter((monster) => {
+            return  monster.name.toLocaleLowerCase().includes(this.state.searchField);
+        });
         return (
             <div className='App'>
                 <input className='search-box' type='search' placeholder='search monsters'
                        onChange={(event) => {
                            console.log(event.target.value);
-                           const searchString = event.target.value.toLocaleLowerCase();
-                           const filteredMonsters = this.state.monsters.filter((monster) => {
-                              return  monster.name.toLocaleLowerCase().includes(searchString);
-                           });
+                           const searchField = event.target.value.toLocaleLowerCase();
                            this.setState(() => {
-                               return {monsters: filteredMonsters};
+                               return {searchField};
                            })
                        }}/>
 
-                {this.state.monsters.map((monster) => {
+                {filteredMonsters.map((monster) => {
                     return (
                         <div key={monster.id}>
                             <h1>{monster.name}</h1>
